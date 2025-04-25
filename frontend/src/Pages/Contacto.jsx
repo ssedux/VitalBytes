6548660b3d9fd3deb7c8dd1fb9e53bb6bbd13611
RobbieLarios2024 <robbieavellana@gmail.com>
2025-04-24 21:32:40 -0600

    Contacto

--- frontend/src/Pages/Contacto.jsx
+++ frontend/src/Pages/Contacto.jsx
@@ -1,13 +1,57 @@
 import React, { useState } from 'react';
+import './style/Contacto.css';
 
 
 function Contacto() {
-    
+  const [formData, setFormData] = useState({
+    nombre: '',
+    apellidos: '',
+    correo: '',
+    mensaje: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Formulario enviado:', formData);
+  };
+
   return (
     <div className="contacto">
-      <h1>Contacto</h1>
+      <div className="contacto-contenido">
+        <div className="contacto-titulo">
+          <h1 className="Contacto">Contacto</h1>
+          <p>¿Tienes alguna pregunta? ¡Déjanos un mensaje!</p>
+        </div>
+  
+        <form onSubmit={handleSubmit} className="formulario-contacto">
+          <div className="campo fila">
+            <label htmlFor="nombre" className="TXT">Nombre:</label>
+            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
+  
+            <label htmlFor="apellidos" className="TXT">Apellidos:</label>
+            <input type="text" name="apellidos" value={formData.apellidos} onChange={handleChange} />
+          </div>
+  
+          <div className="campo">
+            <label htmlFor="correo" className="TXT">Correo:</label>
+            <input type="email" name="correo" value={formData.correo} onChange={handleChange} />
+          </div>
+  
+          <div className="campo">
+            <label htmlFor="mensaje" className="TXT">Mensaje:</label>
+            <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
+          </div>
+  
+          <button type="submit" className="BTT">Enviar</button>
+        </form>
+      </div>
     </div>
   );
+  
 }
 
 export default Contacto;
