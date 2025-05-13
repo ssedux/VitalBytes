06a06a729e1ea949b3fc7c713513c09076217db5
RobbieLarios2024 <robbieavellana@gmail.com>
2025-05-12 19:01:29 -0600

    Cambios hechos por chico hacker

--- frontend/src/Pages/Client/Home.jsx
+++ frontend/src/Pages/Client/Home.jsx
@@ -1,7 +1,5 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
 import Baner from '../../assets/Banner.png';
+import React, { useState } from 'react';
 
 import './../style/Client/Home.css';
 
